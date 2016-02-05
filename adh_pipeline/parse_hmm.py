1c3253d0c6e305cc134a1dd8e8422a2ff38d8d76
Linus Östberg <linus@talavis.eu>
2016-02-05 14:32:19 +0100

    Refactoring and tests for parse_hmm.py
    Made the file usable as a module by moving the running code into a __main__ block.
    
    Tests added for some functions.
    
    .travis.yml updated to include the new tests.

--- adh_pipeline/parse_hmm.py
+++ adh_pipeline/parse_hmm.py
@@ -23,6 +23,12 @@ def rip_data_ncbi(title) :
         accession = parts[3]
     return(accession)
 
+def test_rip_data_ncbi() :
+    assert rip_data_ncbi('>gi|86651|pir||A33909') == 'A33909'
+    assert rip_data_ncbi('>gi|887492973|pdb|5CDS|A') == '5CDS_A'
+    assert rip_data_ncbi('>gi|229330|prf||701201A') == '701201A'
+    assert rip_data_ncbi('>gi|929358743|ref|XP_014088683.1|') == 'XP_014088683.1'
+
 def rip_data_uniprot(title) :
     '''Rip data from a FASTA file from a Uniprot database.
     Returned data will be:
@@ -34,6 +40,9 @@ def rip_data_uniprot(title) :
         return ''
     return accession
 
+def test_rip_data_uniprot() :
+    assert rip_data_uniprot('>tr|W5Q2F4|W5Q2F4_SHEEP') == 'W5Q2F4'
+
 def rip_acc(header) :
     if acc[:2] in ('sp', 'tr') :
         return(rip_data_uniprot(header))
@@ -42,27 +51,28 @@ def rip_acc(header) :
             return(rip_data_ncbi(header))
         except IndexError :
             return(header)
-
-models = list()
-e_values = list()
-acc = ''
-for line in open(table) :
-    if line[0] == '#' :
-        continue
-    cols = line.split()
-    if acc != cols[2] :
-        if acc != '' :
-            outacc = rip_acc(acc)
-            if len(outacc.strip()) > 0 :
-                print('\t'.join((outacc , models[e_values.index(min(e_values))], str(min(e_values)))))
-            else :
-                sys.stderr.write('ERROR: problem with acc ({})\n'.format(acc))
-            models = list()
-            e_values = list()
-        acc = cols[2] 
+        
+if __name__ == '__main__' :
+    models = list()
+    e_values = list()
+    acc = ''
+    for line in open(table) :
+        if line[0] == '#' :
+            continue
+        cols = line.split()
+        if acc != cols[2] :
+            if acc != '' :
+                outacc = rip_acc(acc)
+                if len(outacc.strip()) > 0 :
+                    print('\t'.join((outacc , models[e_values.index(min(e_values))], str(min(e_values)))))
+                else :
+                    sys.stderr.write('ERROR: problem with acc ({})\n'.format(acc))
+                models = list()
+                e_values = list()
+            acc = cols[2] 
     
-    models.append(cols[0])
-    e_values.append(float(cols[4]))
+        models.append(cols[0])
+        e_values.append(float(cols[4]))
     
-if acc != '' :
-    print('\t'.join((rip_acc(acc), models[e_values.index(min(e_values))], str(min(e_values)))))
+    if acc != '' :
+        print('\t'.join((rip_acc(acc), models[e_values.index(min(e_values))], str(min(e_values)))))
