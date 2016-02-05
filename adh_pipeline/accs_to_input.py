c115ddc871962ff97ecfb2ad36b155cd2e97722c
Linus Östberg <linus@talavis.eu>
2016-02-05 12:36:25 +0100

    Tests added to accs_to_input

--- adh_pipeline/accs_to_input.py
+++ adh_pipeline/accs_to_input.py
@@ -15,6 +15,13 @@ def is_uniprot(accession) :
     else :
         return False
 
+def test_is_uniprot() :
+    assert is_uniprot('P41681') == True
+    assert is_uniprot('A0A0F7YFA1') == True
+    assert is_uniprot('ENSG00000172955') == False # Ensembl
+    assert is_uniprot('NP_001095940.1') == False # NCBI
+
+    
 def prepare_data(accession, adh_class, adh_subclass='') :
     '''Prepares data for output in the format for the db.
 
@@ -40,12 +47,18 @@ def prepare_data(accession, adh_class, adh_subclass='') :
         return tuple()
     return species, adh_class, adh_subclass, db, accession, title, sequence
 
+def test_prepare_data() :
+    # stopped working 160204; seems to be a problem in BioPython, not this code
+    #    assert prepare_data('P28332', 5) ==
+    assert prepare_data('NP_001095940.1', 5) == ('Homo sapiens', 5, '', 'NCBI', 'NP_001095940.1', 'alcohol dehydrogenase 6 isoform 1 [Homo sapiens]',
+                                              'MSTTGQVIRCKAAILWKPGAPFSIEEVEVAPPKAKEVRIKVVATGLCGTEMKVLGSKHLDLLYPTILGHEGAGIVESIGEGVSTVKPGDKVITLFLPQCGECTSCLNSEGNFCIQFKQSKTQLMSDGTSRFTCKGKSIYHFGNTSTFCEYTVIKEISVAKIDAVAPLEKVCLISCGFSTGFGAAINTAKVTPGSTCAVFGLGGVGLSVVMGCKAAGAARIIGVDVNKEKFKKAQELGATECLNPQDLKKPIQEVLFDMTDAGIDFCFEAIGNLDVLAAALASCNESYGVCVVVGVLPASVQLKISGQLFFSGRSLKGSVFGGWKSRQHIPKLVADYMAEKLNLDPLITHTLNLDKINEAVELMKTGKCIRCILLL')
+
 if __name__ == '__main__' :
     if len(sys.argv) < 3 or len(sys.argv) > 5 :
         sys.stderr.write('Usage: {0} <file with accessions and classes> <print non-significant instead of significant y/n> [ADH subclass]\n'.format(sys.argv[0]))
         sys.exit()
 
-    ncbi_record.setup('linus.ostberg@ki.se')    
+    ncbi_record.setup('email@example.com')
 
     filename = sys.argv[1]
     if sys.argv[2].lower() not in ('y', 'n') :
@@ -55,13 +68,11 @@ if __name__ == '__main__' :
 
     for line in open(filename) :
         cols = line.split('\t')
-        if float(cols[2]) > 1E-200 :
-            if print_nonsig :
-                res = prepare_data(cols[0], cols[1])
-                if len(res) > 0 :
-                    print('\t'.join(res))
-        else :
-            if not print_nonsig :
-                res = prepare_data(cols[0], cols[1])
-                if len(res) > 0 :
-                    print('\t'.join(res))
+        if float(cols[2]) > 1E-200 and print_nonsig :
+            res = prepare_data(cols[0], cols[1])
+            if len(res) > 0 :
+                print('\t'.join(res))
+        elif not print_nonsig :
+            res = prepare_data(cols[0], cols[1])
+            if len(res) > 0 :
+                print('\t'.join(res))
